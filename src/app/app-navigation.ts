ab7faf29f3e23663f7a2cef586d3c3b75e171222
Alexandre Rodrigues <54284137+alxrds@users.noreply.github.com>
2021-11-23 21:06:18 -0300

    first commit

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,31 @@
+export const navigation = [
+  {
+    text: 'Home',
+    path: '/home',
+    icon: 'home'
+  },
+  {
+    text: 'Examples',
+    icon: 'folder',
+    items: [
+      {
+        text: 'Profile',
+        path: '/profile'
+      },
+      {
+        text: 'Tasks',
+        path: '/tasks'
+      }
+    ]
+  },
+  {
+    text: 'Componente',
+    icon: 'like',
+    items: [
+      {
+        text: 'Componente',
+        path: '/componente'
+      }
+    ]
+  }
+];
---
+++ src/app/shared/components/estado-cidade/estado-cidade.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estado-cidade',
+  templateUrl: './estado-cidade.component.html',
+  styleUrls: ['./estado-cidade.component.scss']
+})
+
+export class EstadoCidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public getEstados(): void{
+
+  }
+
+  public getCidades(): void{
+
+  }
+
+}
